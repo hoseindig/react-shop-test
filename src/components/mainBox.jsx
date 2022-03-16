013c3453d4941d0995dd6cff20505eb4bfd941bd
hoseindig <hosein_dig@yahoo.com>
2022-03-16 16:51:16 +0330

    section

--- src/components/mainBox.jsx
+++ src/components/mainBox.jsx
@@ -1,13 +1,14 @@
 import styles from "./mainBox.module.scss";
 import CarouselBox from "../components/Carousel";
 import CategoryGallery from "./categoryGallery";
+import Section from "./section";
 const MainBox = () => {
   return (
     <div className={styles["main-box"]}>
       <h1>Main Box</h1>
       <CarouselBox />
       <CategoryGallery />
-
+      <Section />
     </div>
   );
 };
---
+++ src/components/section.jsx
@@ -0,0 +1,60 @@
+import styles from "./Section.module.scss";
+import { Card, Button, Container, CardGroup } from "react-bootstrap";
+import CardGroupBox from "./CardGroup";
+const Section = () => {
+  const cards = [
+    {
+      id: 1,
+      title: "Epple",
+      img: "product-4.jpg",
+      text: "  What You Can Learn From Bill Gates",
+    },
+    {
+      id: 2,
+      title: "Hpple",
+      img: "product-5.jpg",
+      text: "a Half Very Simple Things You To",
+    },
+    {
+      id: 3,
+      title: "Bpple",
+      img: "product-8.jpg",
+      text: "  BOOK: Do You Really Need It? This ",
+    },
+    {
+      id: 3,
+      title: "zpple",
+      img: "product-13.jpg",
+      text: "BOOK: Do You Really Need It? This ",
+    },
+ {
+      id: 4,
+      title: "Lpple",
+      img: "product-2.jpg",
+      text: "Revolutionize Your BOOK With ",
+    },
+
+     {
+      id:5,
+      title: "Jpple",
+      img: "product-10.jpg",
+      text: "Revolutionize Your BOOK With ",
+    },
+    {
+      id:6,
+      title: "Wpple",
+      img: "product-3.jpg",
+      text: "Revolutionize Your BOOK With ",
+    },
+  ];
+  return (
+    <div className={styles.box}>
+      <div className={styles.header}>
+        <h1> arts & photography books</h1>
+      </div>
+      <CardGroupBox cards={cards} />
+    </div>
+  );
+};
+
+export default Section;
