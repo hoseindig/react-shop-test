bfb243b82f82b7e542296fd257bf2d09eba8998c
hoseindig <hosein_dig@yahoo.com>
2022-03-17 19:37:20 +0330

    made component

---
+++ src/components/Card/Cardbox.jsx
@@ -0,0 +1,42 @@
+import { Card, Button, Container, CardGroup, Col, Row } from "react-bootstrap";
+import styles from "../ProductsInTowColTab.module.scss";
+const CardBox = ({ item, moneySymbol }) => {
+  return (
+    <Card key={item.id} className={styles["product-card"]}>
+      <Row>
+        <Col md={6}>
+          <img alt="" src={"images/products/" + item.img} />
+        </Col>
+        <Col md={6}>
+          <Card.Body>
+            <Card.Title className={styles["author"]}>
+              {item.title}
+              {/* <p>mostViewedProducts {item.mostViewedProducts.toString()}</p> */}
+              {/* <p>newArrivals {item.newArrivals.toString()}</p> */}
+              {/* <p>featuredProducts {item.featuredProducts.toString()}</p> */}
+              {/* <p> {item.category}</p> */}
+            </Card.Title>
+            <Card.Text className={styles["card-text"]}>
+              <h4>{item.text}</h4>
+            </Card.Text>
+            <Card.Text className={styles["price-block"]}>
+              <span className={styles["price"]}>
+                {moneySymbol}
+                {item.price}
+              </span>
+              <del className={styles["price-old"]}>
+                {moneySymbol}
+                {item.priceOld}
+              </del>
+              <span className={styles["price-discount"]}>
+                {item.priceDiscount}
+              </span>
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default CardBox;
