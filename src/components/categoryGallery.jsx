649368ebc19caf36fc705b9b14f4bc3b3ee1921b
hoseindig <hosein_dig@yahoo.com>
2022-03-17 23:42:19 +0330

    fix category img size

--- src/components/categoryGallery.jsx
+++ src/components/categoryGallery.jsx
@@ -1,31 +1,33 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import styles from "./CategoryGallery.module.scss";
 const CategoryGallery = () => {
   return (
     <div className={styles.category}>
-      <Row>
-        <Col md={3}>
-          <img src="images/CategoryGallery/CategoryGallery1.jpg" alt="" />
-        </Col>
-        <Col md={9}>
-          <Row>
-            <Col md={7}>
-              <img src="images/CategoryGallery/CategoryGallery2.jpg" alt="" />
-            </Col>
-            <Col md={5}>
-              <img src="images/CategoryGallery/CategoryGallery3.jpg" alt="" />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={6}>
-              <img src="images/CategoryGallery/CategoryGallery4.jpg" alt="" />
-            </Col>
-            <Col md={6}>
-              <img src="images/CategoryGallery/CategoryGallery5.jpg" alt="" />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col md={3}>
+            <img src="images/CategoryGallery/CategoryGallery1.jpg" alt="" />
+          </Col>
+          <Col md={9}>
+            <Row>
+              <Col md={7}>
+                <img src="images/CategoryGallery/CategoryGallery2.jpg" alt="" />
+              </Col>
+              <Col md={5}>
+                <img src="images/CategoryGallery/CategoryGallery3.jpg" alt="" />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <img src="images/CategoryGallery/CategoryGallery4.jpg" alt="" />
+              </Col>
+              <Col md={6}>
+                <img src="images/CategoryGallery/CategoryGallery5.jpg" alt="" />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
