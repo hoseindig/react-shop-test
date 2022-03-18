ae0e34706b38bc2a59431652372ac50941d8c3a5
hoseindig <hosein_dig@yahoo.com>
2022-03-18 11:59:52 +0330

    css set line debuge

--- src/components/PromoImage.jsx
+++ src/components/PromoImage.jsx
@@ -5,10 +5,10 @@ const PromoImage = () => {
     <Container>
       <Row className="mt-5 mb-5">
         <Col md={6} xs={12}>
-          <img src="images/PromoImage/promo-banner-with-text.jpg" alt="" />
+          <img src="images/PromoImage/promo-banner-with-text.jpg" alt="" style={{width: "100%"}} />
         </Col> 
         <Col md={6} xs={12}>
-          <img src="images/PromoImage/promo-banner-with-text-2.jpg" alt="" />
+          <img src="images/PromoImage/promo-banner-with-text-2.jpg" alt=""  style={{width: "100%"}}/>
         </Col>
       </Row>
     </Container>
