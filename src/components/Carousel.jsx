4da780a3b3c4e17ee69d51ad2913f6cf588101fd
hoseindig <hosein_dig@yahoo.com>
2022-03-25 19:14:35 +0430

    img => image

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,7 @@
 import { Carousel } from "react-bootstrap";
 import styles from "./Carousel.module.scss";
 const CarouselBox = ({ carousels }) => {
+  console.log("CarouselBox", carousels);
   return (
     <Carousel className={styles.box}>
       {carousels.map((item) => {
@@ -8,7 +9,7 @@ const CarouselBox = ({ carousels }) => {
           <Carousel.Item key={item.img} interval={item.interval}>
             <img
               className={"d-block w-100 " + styles.Image}
-              src={"images/slide/" + item.img}
+              src={"images/slide/" + item.image}
               alt={item.alt}
             />
             <Carousel.Caption className={styles["myCaption"]}>
