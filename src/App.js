4dc08527e0a6968101e068f32b34520a576107ab
hoseindig <hosein_dig@yahoo.com>
2022-03-26 13:23:45 +0430

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 
 import "./App.scss";
 import TopNavbar from "./components/topNavbar";
 import NavbarForm from "./components/navbar";
-import MainMenuNavbar from "./components/mainMenuNavbar";
+// import MainMenuNavbar from "./components/mainMenuNavbar";
 import UserProvider from "./store/userProvider";
-import QuickMenu from "./components/quickMenu";
+// import QuickMenu from "./components/quickMenu";
 
 import MainBox from "./components/mainBox";
 import Login from "./components/Login";
+import ProductDetails from "./components/productDetails";
 import Footer from "./components/footer";
 
+
 function App() {
-  const userData = useState({
-    usewrname: "",
-    password: "",
-    fullName: "hossein sheykhi",
-  });
   // style={{height: "calc(100vh - 344px)"}}
   return (
     <UserProvider>
@@ -34,20 +30,21 @@ function App() {
           </Row>
           {/* <MainMenuNavbar /> */}
           {/* <Row> */}
-            <Routes>
-              <Route
-                path="/"
-                exec
-                element={
-                  <Row>
-                    <Col md={12}>
-                      <MainBox />
-                    </Col>
-                  </Row>
-                }
-              />
-              <Route path="/login" element={<Login />} />
-            </Routes>
+          <Routes>
+            <Route
+              path="/"
+              exec
+              element={
+                <Row>
+                  <Col md={12}>
+                    <MainBox />
+                  </Col>
+                </Row>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
+          </Routes>
           {/* </Row> */}
           <Row>
             <Footer />
