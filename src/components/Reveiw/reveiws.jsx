3e9860199633f4fa9f5f9d9761c381c96a0909a3
hoseindig <hosein_dig@yahoo.com>
2022-03-27 20:26:47 +0430

    ReveiwsBox

---
+++ src/components/Reveiw/reveiws.jsx
@@ -0,0 +1,34 @@
+import Rate from "../rate/rate";
+import styles from "./Review.module.scss";
+const ReveiwsBox = ({ reviews,  item }) => {
+  return (
+    <div>
+      <h4 className={styles["title-lg"]}>
+        {reviews.length} REVIEW FOR {item.title}
+      </h4>
+      {reviews.map((review) => {
+        return (
+          <div key={review._id}>
+            <div className={styles["review-comment"]}>
+              <div className={styles["avatar"]}>
+                <img src="/images/userImage/author-logo.jpg" alt="" />
+              </div>
+              <div className={styles["text"]}>
+                <div className="rating-block mb--15">
+                  <Rate rate={review.rate} noText={true} />
+                </div>
+                <h6 className="author">
+                  {review.userName} –{" "}
+                  <span className="font-weight-400">{review.dateTime}</span>
+                </h6>
+                <p>{review.text}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ReveiwsBox;
