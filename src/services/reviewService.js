a48d8c4439008d36d54ded29825c351e8d8ede65
hoseindig <hosein_dig@yahoo.com>
2022-03-28 21:46:10 +0430

    saveReview

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -8,7 +8,8 @@ const reviews = [
     userId: 1,
     rate: 2,
     userName: "ADMIN",
-    userImage: 1,
+    userImage: "author-logo.jpg",
+
     dateTime: "March 23, 2015",
     text: "Lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi.",
   },
@@ -19,7 +20,8 @@ const reviews = [
     userId: 1,
     rate: 2,
     userName: "ADMIN",
-    userImage: 1,
+    userImage: "author-logo.jpg",
+
     dateTime: "March 23, 2015",
     text: "Lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi.",
   },
@@ -30,7 +32,8 @@ const reviews = [
     userId: 1,
     rate: 2,
     userName: "ADMIN",
-    userImage: 1,
+    userImage: "author-logo.jpg",
+
     dateTime: "March 23, 2015",
     text: "Lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi.",
   },
@@ -41,7 +44,8 @@ const reviews = [
     userId: 1,
     rate: 2,
     userName: "ADMIN",
-    userImage: 1,
+    userImage: "author-logo.jpg",
+
     dateTime: "March 23, 2015",
     text: "Lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi.",
   },
@@ -52,18 +56,18 @@ const reviews = [
     userId: 1,
     rate: 2,
     userName: "ADMIN 1",
-    userImage: 1,
+    userImage: "author-logo.jpg",
     dateTime: "March 23, 2019",
     text: "Lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi.",
   },
-   {
+  {
     _id: "5b21ca3eeb7f6fbccd471816",
     title: "Terminator 6",
     productId: 5,
     userId: 1,
     rate: 4,
     userName: "ADMIN 2",
-    userImage: 1,
+    userImage: "author-logo.jpg",
     dateTime: "March 23, 2021",
     text: "Lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi.",
   },
@@ -83,10 +87,19 @@ export function getReviewByProductId(id) {
 
 export function saveReview(review) {
   let reviewInDb = reviews.find((m) => m._id === review._id) || {};
+  reviewInDb = { ...review, ...reviewInDb };
   reviewInDb.title = review.name ? review.name : review.title;
+
+  const today = new Date();
+  const date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date + " " + time;
+  reviewInDb.dateTime = dateTime;
   //   reviewInDb.genre = genresAPI.genres.find((g) => g._id === review.genreId);
-  reviewInDb.numberInStock = review.numberInStock;
-  reviewInDb.dailyRentalRate = review.dailyRentalRate;
+  // reviewInDb.numberInStock = review.numberInStock;
+  // reviewInDb.dailyRentalRate = review.dailyRentalRate;
 
   if (!reviewInDb._id) {
     reviewInDb._id = uuidv4(); //Date.now();
